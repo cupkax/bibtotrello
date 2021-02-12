154c038a864bad9eb7dec839215fccb798a26ba4
Vedant <vedantsansare23@gmail.com>
2021-02-12 12:16:16 +0530

    Import file in working dir

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
+import os
+import shutil
+
 import bibtexparser
 from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import convert_to_unicode
 
-title_list = []
 
-
-def get_all_titles(filename):
-    file = open(filename, encoding="utf8")
-    parser = BibTexParser(common_strings=True)
-    parser.customization = convert_to_unicode
-    bib_database = bibtexparser.load(file, parser=parser)
+def get_titles(bib_dir):
+    # Get bibtex file in current working directory
+    shutil.copy(
+        str(os.path.join(os.path.expanduser('~'), bib_dir)),
+        str(os.getcwd()))
+    bib_data = open('zotLib.bib', encoding='utf-8')
+    t = []
+    parser = BibTexParser(common_strings=True)  # common_strings parameters is required to parse months
+    parser.customization = convert_to_unicode  # latex to unicode
+    # Load the bibtex file
+    bib_database = bibtexparser.load(bib_data, parser=parser)
+    # Iterate through the bibtex and append titles to list
     for i in range(0, len(bib_database.entries)):
-        title_list.append(bib_database.entries[i].get("title"))
+        t.append(bib_database.entries[i].get("title"))
+    return t
 
 
-get_all_titles('zotLib.bib')
+get_titles('Dropbox/org/research/zotLib.bib')
